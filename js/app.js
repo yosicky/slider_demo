b109632588edc09e24cb04deb67a1ba1b54aabf2
yosicky <yosicky.ts@gmail.com>
2016-07-05 07:11:26 +0900

    mod js
    独自スライド送り

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 $(function () {
     var countup = function () {
         console.log('Q');
+        $('.flexslider').flexslider('next')
         setTimeout(countup, 2000);
     };
     $(window).on('load', function () {
         $('.flexslider').flexslider({
             animation: 'slide',
             start: function () {
-                console.log('load');
+                console.log('loaded');
                 countup();
             }
         });
+    });
+    $('.flexslider').on('hover', () => {
+        console.log('hover');
     });
 
 });
