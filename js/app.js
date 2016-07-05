2abe6ad27565de11ec9128a76bb0b8f4145a179e
yosicky <yosicky.ts@gmail.com>
2016-07-06 07:17:37 +0900

    mod js
    タイマー改造

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 $(function () {
+    var count = 0;
+    var countUpId = null
+    var timerId = null;
     var countup = function () {
+        count += 1000/30;
         console.log('Q');
-        $('.flexslider').flexslider('next')
-        setTimeout(countup, 2000);
+        console.log(count);
+        countUpId = setTimeout(countup, 1000/30);
+        if (count >= 5000) {
+            count = 0;
+//            clearTimeout(timerId);
+            console.log('Time!');
+            $('.flexslider').flexslider('next');
+        }
     };
     $(window).on('load', function () {
         $('.flexslider').flexslider({
@@ -13,8 +23,31 @@ $(function () {
             }
         });
     });
-    $('.flexslider').on('hover', () => {
+
+    var counter = function () {
+        count += 1000/30;
+        console.log(count);
+        timerId = setTimeout(counter, 1000/30);
+        if (count >= 5000) {
+            count = 0;
+//            clearTimeout(timerId);
+            console.log('Time!');
+        }
+    };
+//    counter();
+
+    $('.flexslider').on('mouseover', () => {
         console.log('hover');
+        clearTimeout(countUpId);
+//        clearTimeout(timerId);
+        console.log(count);
     });
+    $('.flexslider').on('mouseleave', () => {
+        console.log('leave');
+        countup();
+//        counter();
+        console.log(count);
+    });
+
 
 });
